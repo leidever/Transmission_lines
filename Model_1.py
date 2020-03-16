9158e0aecea54b74c527bf55e6af86045ba058bb
Alexey Diachenko <a.diachenko@gmail.com>
2020-03-17 01:11:24 +0300

    Need to plot everything in meters or Herts

--- Model_1.py
+++ Model_1.py
@@ -1,44 +1,49 @@
-'''from cmath import sqrt'''
-from __future__ import division
+#from __future__ import division
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 plt.rcParams.update({'font.size': 16})
 
+# speed of light, source frequency and wave length
+с = 3.0E8; f = 10.0E6; l_0 = с/f;
 
-Z_L = 75.00
-Z_T = 61.24
-Z_0 = 50.00
+# gamma = alpha + j*beta - propagation constant
+alpha = 0
+beta=2*np.pi/l_0
+
+# Load, transmission (transformator unit), source (0)
+Z_L = 75.0
+Z_0 = 50.0
+Z_T = np.sqrt(Z_0*Z_L)
+
 
 #def gamma(x):
 #    gamma = (Z_T*Z_L-Z_0*Z_T + 1j*(Z_T*Z_T-Z_0*Z_L)/np.tan(x)) / (Z_T*Z_L+Z_0*Z_T + 1j*(Z_T*Z_T+Z_0*Z_L)/np.tan(x))
 #    return abs(gamma)
 
-def Z_in(x):
-    Z_in = Z_T*((Z_L - 1j*Z_T*np.tanh(x))/(Z_T-1j*Z_L*np.tanh(x)))
-    return Z_in
+# All losses are zero, use tan instead of tanh
+def Z_in(l):
+    Z_in = Z_T*((Z_L - 1j*Z_T*np.tan(beta*l))/(Z_T-1j*Z_L*np.tan(beta*l)))
+    return abs(Z_in)
 
-
+'''
 def gamma(x):
     gamma = (Z_in(x) - Z_0) / (Z_in(x) + Z_0)
     return abs(gamma)
-
-
+'''
 
 N = 100
-x = np.linspace(-10.0, 10., N)
+x = np.linspace(0.0, l_0, N)
 
-y = gamma(x)
+y = Z_in(x)
 
 fig = plt.figure()
-plt.plot(x, y, label = "gamma")
-#plt.xlabel('epsilon')
-#plt.ylabel('Г (коэф. отражения)')
+plt.plot(x, y, label = "Zin")
+plt.xlabel('l, meters')
+plt.ylabel('Z_in, Ohm')
 
 plt.legend()
 plt.grid(True)
 
 plt.show()
-
-
